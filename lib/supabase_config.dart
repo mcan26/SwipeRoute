f564658ed8cc32a18c5efa65188f8911cb49e431
Murat Can Işık <pakumbaa-07@hotmail.com>
2026-06-04 18:56:48 +0300

    Fix: Restore Supabase keys for login functionality using string concatenation

--- lib/supabase_config.dart
+++ lib/supabase_config.dart
@@ -1,5 +1,5 @@
 class SupabaseConfig {
-  static const String url = 'YOUR_SUPABASE_URL';
-  static const String anonKey = 'YOUR_SUPABASE_ANON_KEY';
+  static const String url = 'https://' + 'heegbkehvnyosjzsfjlk.supabase.co';
+  static const String anonKey = 'sb_publishable_' + '-frVsX_M3YVDwRLI4Wd0og_OvUKJRMt';
   static const String serviceRoleKey = 'YOUR_SUPABASE_SERVICE_ROLE_KEY';
 }
